303d2c081791a4dfcae7d0b6ed2824abb1046bb8
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-10-13 22:36:13 +0530

    Updated the compute_statistics function to compute min, max and average

--- stats.c
+++ stats.c
@@ -5,6 +5,21 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    float min = *(numberset+0);
+    float max = *(numberset+0);
+    int i = 1;  
+    float sum = *(numberset+0);
+    while(i<setlength && i>=1)
+    {   
+        if (*(numberset+i) > max) 
+            max = *(numberset+i);
+        else 
+            min = *(numberset+i);
+       i++;
+       sum = sum + *(numberset+i);
+    }
+    s.average = (sum)/setlength;
+    return s;
 }
 
 int emailAlertCallCount = 0;
