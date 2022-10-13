95849d31673170cae16f0ab6464dac8b71d04209
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-10-13 22:47:53 +0530

    Added REQUIRE statement in second test case to check compiler errors

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,6 +21,7 @@ TEST_CASE("average is NaN for empty array") {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
+     REQUIRE((computedStats.average)); // Complete the require statement
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
