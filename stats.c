abadf268b2c92501f674ee3e29c308585ece06fa
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-10-18 00:07:58 +0530

    Yet to modify check_and_alert method

--- stats.c
+++ stats.c
@@ -38,8 +38,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-/*void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
     //Update the method for check_and_alert function
     
+    
 }
-*/
