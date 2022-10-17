2953c787594c0a2d5a0be0adbd20c0885229262f
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-10-17 22:50:21 +0530

    Added NAN require statements

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,9 +21,13 @@ TEST_CASE("average is NaN for empty array") {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
-     REQUIRE((computedStats.average)); // Complete the require statement
+     // Complete the require statement
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
+    REQUIRE(isnan(computedStats.average));
+    REQUIRE(isnan(computedStats.max));
+    REQUIRE(isnan(computedStats.min));
+    
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
