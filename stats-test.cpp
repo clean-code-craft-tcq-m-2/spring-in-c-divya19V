bbb51b5c0153e5e90a73bc6f37da955f5fb0d638
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-10-18 00:05:05 +0530

    Working on events concept in C

--- stats-test.cpp
+++ stats-test.cpp
@@ -32,17 +32,17 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("raises alerts when max is greater than threshold") {
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
-    //alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
+    alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
-    //float numberset[] = {99.8, 34.2, 4.5};
-    //int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    //Stats computedStats = compute_statistics(numberset, setlength);
+    float numberset[] = {99.8, 34.2, 4.5};
+    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    Stats computedStats = compute_statistics(numberset, setlength);
 
-    //const float maxThreshold = 10.2;
-    //check_and_alert(maxThreshold, alerters, computedStats);
+    const float maxThreshold = 10.2;
+    check_and_alert(maxThreshold, alerters, computedStats);
 
     // need a way to check if both emailAlerter, ledAlerter were called
     // you can define call-counters along with the functions, as shown below
-    //REQUIRE(emailAlertCallCount == 1);
-    //REQUIRE(ledAlertCallCount == 1);
+    REQUIRE(emailAlertCallCount == 1);
+    REQUIRE(ledAlertCallCount == 1);
 }
